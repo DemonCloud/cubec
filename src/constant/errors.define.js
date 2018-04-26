37afd9dc11bec330d97b4f515a1d1624e46475f6
YiJun <cloudywolf1@qq.com>
2018-04-26 15:32:52 +0800

    merge

--- src/constant/errors.define.js
+++ src/constant/errors.define.js
@@ -1,5 +1,7 @@
 const ERRORS = {
-  MODEL_UNEXPECT: "unexpect c.model with only data that accepts a standard JSON type object."
+  MODEL_UNEXPECT: "unexpect c.model with only data that accepts a standard JSON type object.",
+  ATOM_UNDEFINED_MODELNAME: "unexpect model name that atom can find it",
+  ATOM_MISSING_MODELINSTANCE: "can not find target modal with atom.swap"
 };
 
 export default ERRORS;
