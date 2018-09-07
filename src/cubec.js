f56588eb239fe4d97b0dd68de7b16ba75316ff6b
YiJun <cloudywolf1@qq.com>
2018-09-07 09:55:46 +0800

    rename for c

---
+++ src/cubec.js
@@ -0,0 +1,26 @@
+import model from './packages/model';
+import view from './packages/view';
+import atom from './packages/atom';
+import verify from './packages/verify';
+import struct from './lib/struct';
+
+import createC from './utils/createC';
+import createExtend from './utils/createExtend';
+
+const cubec = Object.create(null);
+
+cubec.version = "1.0.0";
+cubec.struct = struct;
+cubec.verify = verify;
+
+// create module
+cubec.atom = createC(atom);
+cubec.view = createC(view);
+cubec.model = createC(model);
+
+// create Extra option
+cubec.atom.extend = createExtend(atom);
+cubec.view.extend = createExtend(view);
+cubec.model.extend = createExtend(model);
+
+export default Object.freeze(cubec);
