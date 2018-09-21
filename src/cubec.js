e34e8020b020156905c69dba70adb4a94b2c9d62
YiJun <cloudywolf1@qq.com>
2018-09-21 11:25:09 +0800

    add Router packages

--- src/cubec.js
+++ src/cubec.js
@@ -1,6 +1,7 @@
 import model from './packages/model';
 import view from './packages/view';
 import atom from './packages/atom';
+import router from './packages/router';
 import verify from './packages/verify';
 import struct from './lib/struct';
 
@@ -17,10 +18,12 @@ cubec.verify = verify;
 cubec.atom = createC(atom);
 cubec.view = createC(view);
 cubec.model = createC(model);
+cubec.router = createC(router);
 
 // create Extra option
 cubec.atom.extend = createExtend(atom);
 cubec.view.extend = createExtend(view);
 cubec.model.extend = createExtend(model);
+cubec.router.extend = createExtend(router);
 
 export default Object.freeze(cubec);
