eb3f476e27df7df28500e1f0727655db82f13b0a
YiJun <cloudywolf1@qq.com>
2019-11-05 10:01:44 +0800

    upgrade types and linkSystem 1.8.6

--- src/cubec.js
+++ src/cubec.js
@@ -11,7 +11,7 @@ import { createC, createExtend } from './utils/create';
 const cubec = Object.create(null);
 
 // information
-cubec.version = "1.8.5";
+cubec.version = "1.8.6";
 
 _view.__instance[0] = _model;
 _view.__instance[1] = _atom;
