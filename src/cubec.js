db830902806d72f9fd9514f53db8ceb510ae068d
YiJun <cloudywolf1@qq.com>
2020-04-01 00:35:23 +0800

    feat: version 1.9.12

--- src/cubec.js
+++ src/cubec.js
@@ -9,7 +9,7 @@ import { createC, createExtend } from './utils/create';
 export const cubec = Object.create(null);
 
 // information
-cubec.version = "1.9.10";
+cubec.version = "1.9.12";
 cubec.author = "YiJun";
 
 // enter instance
