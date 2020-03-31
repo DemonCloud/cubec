48ae867d977506f43202b90bd9437a5cb2f6e191
YiJun <cloudywolf1@qq.com>
2020-04-01 00:33:23 +0800

    feat: fix IE detector

--- src/utils/adapter.js
+++ src/utils/adapter.js
@@ -1,5 +1,31 @@
+// adapter for IE
 // FUCK IE9
 // JUST FUCK Internet Explorer, FUCK!!!
-const UA = window.navigator.userAgent.toLowerCase();
+function IEVersion() {
+  const ua = window.navigator.userAgent;
 
-export const isIE = (UA.indexOf('msie') !== -1) ? parseInt(UA.split('msie')[1]) : false;
+  const msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  const trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    const rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  // skip Edge
+  // const edge = ua.indexOf('Edge/');
+  // if (edge > 0) {
+  //   // Edge => return version number
+  //   return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  // }
+
+  // other browser
+  return 0;
+}
+
+export const isIE = IEVersion();
